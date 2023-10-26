65b0c7cb78d9fca64d24f7ab0c6f073c121615d2
EC2 Default User <ec2-user@ip-172-31-45-100.ap-southeast-2.compute.internal>
2023-10-26 07:01:41 +0000

    演算子重なり修正

--- main.js
+++ main.js
@@ -2,11 +2,162 @@ function clickbutton(target) {
 let result = document.getElementById("result")
 let targetValue = target.innerHTML;
 
+
 if(targetValue == "AC"){
   result.innerHTML ="0"
 }
 else if(targetValue == "="){
   result.innerHTML = eval(result.innerHTML)
+}
+else if(result.innerHTML.slice(-1) == "+"){
+  if(targetValue == "+"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "-"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "*"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "/"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "."){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "00"){
+  let result3 = result.innerHTML
+  result.innerHTML = result3
+  }
+  
+  else{
+  result.innerHTML += targetValue
+  }
+}
+else if(result.innerHTML.slice(-1) == "-"){
+  if(targetValue == "+"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "-"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "*"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "/"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "."){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "00"){
+  let result3 = result.innerHTML
+  result.innerHTML = result3
+  }
+  
+  else{
+  result.innerHTML += targetValue
+  }
+}
+else if(result.innerHTML.slice(-1) == "*"){
+  if(targetValue == "+"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "-"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "*"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "/"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "."){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "00"){
+  let result3 = result.innerHTML
+  result.innerHTML = result3
+  }
+  
+  else{
+  result.innerHTML += targetValue
+  }
+}
+else if(result.innerHTML.slice(-1) == "/"){
+  if(targetValue == "+"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "-"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "*"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "/"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "."){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "00"){
+  let result3 = result.innerHTML
+  result.innerHTML = result3
+  }
+  
+  else{
+  result.innerHTML += targetValue
+  }
+}
+else if(result.innerHTML.slice(-1) == "."){
+  if(targetValue == "+"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "-"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "*"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "/"){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "."){
+  let result2 = result.innerHTML.slice(0,-1)
+  result.innerHTML = result2 + targetValue
+  }
+  else if(targetValue == "00"){
+  let result3 = result.innerHTML
+  result.innerHTML = result3
+  }
+  
+  else{
+  result.innerHTML += targetValue
+  }
 }
 else{
   if(result.innerHTML =="0"){
